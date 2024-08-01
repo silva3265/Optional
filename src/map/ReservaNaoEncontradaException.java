1b46c3deef6fea515c7fcc57c6ed795d10b214a7
silva3265 <silva3265@gmail.com>
2024-08-01 00:06:24 -0300

    Aplicando transformações com map

---
+++ src/map/ReservaNaoEncontradaException.java
@@ -0,0 +1,12 @@
+package map;
+
+public class ReservaNaoEncontradaException extends RuntimeException {
+
+    public ReservaNaoEncontradaException() {
+    }
+
+    public ReservaNaoEncontradaException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/map/Voo.java
@@ -0,0 +1,4 @@
+package map;
+
+public record Voo(String codigo, String origem, String destino) {
+}
